06fd32b93f71825f1f518100118ae618ba6cecae
Björn Petersen <pheasn@gmail.com>
2019-05-12 13:01:19 +0200

    Delegate songs with video IDs to YouTube (#18)
    * Delegate songs with video IDs to YouTube
    
    Closes #17
    
    * Fix CircleCI save_cache key
    
    * Execute build after check in CircleCI

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -3,6 +3,7 @@ object Lib {
   const val SLF4J = "1.8.0-beta4"
 
   const val MUSICBOT = "0.21.0"
+  const val YOUTUBE_PROVIDER = "0.16.0"
   const val GPLAYMUSIC = "0.3.6"
 
   const val JUNIT = "5.4.2"
